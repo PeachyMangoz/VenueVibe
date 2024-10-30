2b1caafe4aa867e4dca5284502fb1e8ee1b9b599
Isis Eka Kesuma <isis.ek.2023@scis.smu.edu.sg>
2024-10-30 18:55:03 +0800

    Update main.js

--- VenueVibe/frontend/src/main.js
+++ VenueVibe/frontend/src/main.js
@@ -1,4 +1,24 @@
 // src/main.js
+// import { createApp } from 'vue';
+// import App from './App.vue';
+// import router from './router';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap';
+// import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
+
+// import { library } from '@fortawesome/fontawesome-svg-core';
+// import {fas} from '@fortawesome/free-solid-svg-icons';
+// import {fab} from '@fortawesome/free-brands-svg-icons';
+// import {far} from '@fortawesome/free-regular-svg-icons';
+
+// library.add(fas,far,fab);
+
+// createApp(App)
+// .use(router)
+// .component("fa", FontAwesomeIcon)
+// .mount('#app');
+
+// src/main.js
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
@@ -13,13 +33,28 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
+
+// Import Font Awesome CSS
 import '@fortawesome/fontawesome-svg-core/styles.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+// Initialize AOS ( for animation )
+AOS.init({
+    // Global settings:
+    duration: 800,
+    easing: 'ease-in-out',
+    once: true,
+    mirror: false
+  });
 
 // Add icons to the library
 library.add(fas, far, fab);
 
-
+// Create app
 const app = createApp(App);
+
+// Use router
 app.use(router);
 
 // Register Font Awesome component globally
@@ -27,4 +62,5 @@ app.use(router);
 app.component('fa', FontAwesomeIcon);
 app.component('font-awesome-icon', FontAwesomeIcon); // Optional: register with full name too
 
+// Mount app
 app.mount('#app');
