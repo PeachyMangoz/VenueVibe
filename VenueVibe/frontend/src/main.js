de12e4e18a36c344e952c2e77b07067678e02779
Isis Eka Kesuma <isis.ek.2023@scis.smu.edu.sg>
2024-10-30 18:57:14 +0800

    Update main.js

--- VenueVibe/frontend/src/main.js
+++ VenueVibe/frontend/src/main.js
@@ -1,22 +1,4 @@
-// src/main.js
-// import { createApp } from 'vue';
-// import App from './App.vue';
-// import router from './router';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap';
-// import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
 
-// import { library } from '@fortawesome/fontawesome-svg-core';
-// import {fas} from '@fortawesome/free-solid-svg-icons';
-// import {fab} from '@fortawesome/free-brands-svg-icons';
-// import {far} from '@fortawesome/free-regular-svg-icons';
-
-// library.add(fas,far,fab);
-
-// createApp(App)
-// .use(router)
-// .component("fa", FontAwesomeIcon)
-// .mount('#app');
 
 // src/main.js
 import { createApp } from 'vue';
