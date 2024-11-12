31423b1dc52edc358f1b1fee62e5f1fdf9c4f112
tasmin <txsminn@gmail.com>
2024-11-13 00:02:34 +0800

    fixed chat btn

--- VenueVibe/frontend/src/App.vue
+++ VenueVibe/frontend/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <head>
-    <meta charset="utf-8">
-    <link rel="icon" href="/VenueVibe/frontend/public/favicon.ico">
+    <meta charset="utf-8" />
+    <link rel="icon" href="/VenueVibe/frontend/public/favicon.ico" />
     <title>Boothy</title>
-</head>
+  </head>
   <div id="app">
     <header></header>
 
@@ -11,39 +11,79 @@
 
     <main class="main-content">
       <router-view />
+      <!-- Chat Icon Button -->
+      <div v-if="isLoggedIn" class="chat-icon">
+      <router-link to="/CollaborateChat">
+        <fa :icon="['fas', 'comment-dots']" style="color: #ffffff;"></fa>
+      </router-link>
+    </div>
     </main>
     <!-- SORRY I HAVENT ADD ALL THE LINKS -->
     <footer id="footer" class="footer">
+      <div class="container footer-top">
+        <div class="row gy-4">
+          <div class="col-lg-4 col-md-6 footer-about">
+            <h5 class="display-6 fw-bold" style="font-weight: 600">Boothy</h5>
+            <div class="footer-contact pt-3">
+              <p>81 Victoria St</p>
+              <p>Singapore, S188065</p>
+              <p class="mt-3">
+                <strong>Phone:</strong> <span>+1 5589 55488 55</span>
+              </p>
+              <p>
+                <strong>Email:</strong>
+                <span>ritz.chong.2022@scis.smu.edu.sg</span>
+              </p>
+            </div>
+            <div class="social-links d-flex mt-4">
+              <a href="https://twitter.com/sgSMU"
+                ><i class="bi bi-twitter-x"></i
+              ></a>
+              <a href="https://www.facebook.com/timothy.swee?mibextid=LQQJ4d"
+                ><i class="bi bi-facebook"></i
+              ></a>
+              <a href="https://www.instagram.com/thisisisisshop/"
+                ><i class="bi bi-instagram"></i
+              ></a>
+              <a href="https://www.linkedin.com/in/tasminfoo/"
+                ><i class="bi bi-linkedin"></i
+              ></a>
+            </div>
+          </div>
 
-<div class="container footer-top">
-  <div class="row gy-4">
-    <div class="col-lg-4 col-md-6 footer-about">
-      <h5 class=" display-6 fw-bold" style="font-weight: 600">Boothy</h5>
-      <div class="footer-contact pt-3">
-        <p> 81 Victoria St</p>
-        <p> Singapore, S188065</p>
-        <p class="mt-3"><strong>Phone:</strong> <span>+1 5589 55488 55</span></p>
-        <p><strong>Email:</strong> <span>ritz.chong.2022@scis.smu.edu.sg</span></p>
-      </div>
-      <div class="social-links d-flex mt-4">
-        <a href="https://twitter.com/sgSMU"><i class="bi bi-twitter-x"></i></a>
-        <a href="https://www.facebook.com/timothy.swee?mibextid=LQQJ4d"><i class="bi bi-facebook"></i></a>
-        <a href="https://www.instagram.com/thisisisisshop/"><i class="bi bi-instagram"></i></a>
-        <a href="https://www.linkedin.com/in/tasminfoo/"><i class="bi bi-linkedin"></i></a>
-      </div>
-    </div>
+          <div class="col-lg-4 col-md-6 footer-links">
+            <h5>Useful Links</h5>
+            <ul>
+              <li>
+                <router-link to="/" class="text-decoration-none text-white"
+                  >Home</router-link
+                >
+              </li>
+              <li>
+                <router-link
+                  to="/booths"
+                  class="text-decoration-none text-white"
+                  >Booth Listings</router-link
+                >
+              </li>
+              <li>
+                <router-link
+                  to="/eventreviews"
+                  class="text-decoration-none text-white"
+                  >Event Reviews</router-link
+                >
+              </li>
+              <li>
+                <router-link
+                  to="/collaborate"
+                  class="text-decoration-none text-white"
+                  >Collaborate</router-link
+                >
+              </li>
+            </ul>
+          </div>
 
-    <div class="col-lg-4 col-md-6 footer-links">
-      <h5>Useful Links</h5>
-      <ul>
-        <li><router-link to="/" class="text-decoration-none text-white">Home</router-link></li>
-        <li><router-link to="/booths" class="text-decoration-none text-white">Booth Listings</router-link></li>
-        <li><router-link to="/eventreviews" class="text-decoration-none text-white">Event Reviews</router-link></li>
-        <li><router-link to="/collaborate" class="text-decoration-none text-white">Collaborate</router-link></li>
-      </ul>
-    </div>
-
-    <!-- <div class="col-lg-2 col-md-3 footer-links">
+          <!-- <div class="col-lg-2 col-md-3 footer-links">
       <h5>Our Services</h5>
       <ul>
         <li><a href="#" class="text-decoration-none text-white">Web Design</a></li>
@@ -54,43 +94,55 @@
       </ul>
     </div> -->
 
-    <div class="col-lg-4 col-md-12 footer-newsletter">
-      <h5>Our Newsletter</h5>
-      <p>Subscribe to our newsletter and receive the latest news about our products and services!</p>
-      <form action="forms/newsletter.php" method="post" class="php-email-form">
-        <div class="newsletter-form"><input type="email" name="email"><input type="submit" value="Subscribe"></div>
-        <!-- <div class="loading">Loading</div>
+          <div class="col-lg-4 col-md-12 footer-newsletter">
+            <h5>Our Newsletter</h5>
+            <p>
+              Subscribe to our newsletter and receive the latest news about our
+              products and services!
+            </p>
+            <form
+              action="forms/newsletter.php"
+              method="post"
+              class="php-email-form"
+            >
+              <div class="newsletter-form">
+                <input type="email" name="email" /><input
+                  type="submit"
+                  value="Subscribe"
+                />
+              </div>
+              <!-- <div class="loading">Loading</div>
         <div class="error-message"></div>
         <div class="sent-message">Your subscription request has been sent. Thank you!</div> -->
-      </form>
-    </div>
-    <p style="text-align:center;">© 2024 Boothy</p>
+            </form>
+          </div>
+          <p style="text-align: center">© 2024 Boothy</p>
+        </div>
+      </div>
+    </footer>
 
+    <!-- Scroll Top -->
+    <!-- <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a> -->
+
+    <!-- Preloader -->
+    <div id="preloader"></div>
   </div>
-</div>
-
-</footer>
-
-<!-- Scroll Top -->
-<!-- <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a> -->
-
-<!-- Preloader -->
-<div id="preloader"></div>
-
-</div>
 </template>
 
 <script>
-import NavBar from './components/NavBar.vue';
+import NavBar from "./components/NavBar.vue";
+import { mapGetters } from "vuex";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     NavBar,
   },
+  computed: {
+    ...mapGetters(['isLoggedIn']),
+  },
 };
 </script>
-
 
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Alkatra:wght@400..700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
@@ -114,13 +166,13 @@ export default {
   /* padding: 30px; */
   max-width: 100%;
   margin: 0 auto;
-  font-family: 'Roboto', sans-serif;
+  font-family: "Roboto", sans-serif;
   padding-top: 80px; /* Adjust this based on your nav bar height */
 }
 .footer {
   position: relative; /* Ensure footer is positioned relative to the main content */
   z-index: 10; /* Make sure it appears above the main content */
-  color: white; 
+  color: white;
   background-color: black;
   padding: 50px 0; /* Changed to vertical padding only */
   width: 100%;
@@ -128,9 +180,9 @@ export default {
   min-height: 200px;
 }
 
-.footer h5{
-  color:beige;
-    /* margin-bottom: 10px; */
+.footer h5 {
+  color: beige;
+  /* margin-bottom: 10px; */
   font-weight: 700;
 }
 
@@ -138,8 +190,8 @@ export default {
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 40px;  /* Adjust size as needed */
-  height: 40px;  /* Adjust size as needed */
+  width: 40px; /* Adjust size as needed */
+  height: 40px; /* Adjust size as needed */
   border-radius: 50%;
   border: 1px solid rgba(255, 255, 255, 0.2);
   color: white;
@@ -153,7 +205,24 @@ export default {
 }
 
 .social-links i {
-  font-size: 18px;  /* Adjust icon size as needed */
+  font-size: 18px; /* Adjust icon size as needed */
+}
+
+.chat-icon {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  background-color: rgb(54, 181, 152); /* Change to your preferred color */
+  border-radius: 50%;
+  width: 60px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.3);
+  cursor: pointer;
+  z-index: 1000; /* Ensure it appears on top of other elements */
+  font-size: 25px;
 }
 
 /* .footer .social-media {
